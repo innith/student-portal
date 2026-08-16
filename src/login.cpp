b194a35c12921ef6f7c5eea6db07609db70c4c77
Nithin Reddy Bathula <nithinreddybathula.i@gmail.com>
2026-08-16 10:37:48 +0530

    Added admin authantication logic for further inplementation in login.cpp file

--- src/login.cpp
+++ src/login.cpp
@@ -11,3 +11,10 @@ bool validatePassword(const string &password)
 {
   return (password.length() >= 6);
 }
+
+bool isAdmin(const string &username, const string &password)
+{
+  const string actualUsername = "admin";
+  const string actualPassword = "admin@123";
+  return (username == actualUsername) && (password == actualPassword);
+}
